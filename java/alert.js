9a800155c384d3f92154c44cf84b4e3625d694bc
Rodrigo Joaquim Luz <109965297+rodrigo-luz2022@users.noreply.github.com>
2022-08-08 21:20:43 -0300

    Delete java directory

--- java/alert.js
+++
@@ -1,9 +0,0 @@
-let btnShow = document.querySelector('button');
-btnShow.addEventListener('click', () => {
-    swal({
-        title: 'Atraso Escolar',
-        text: 'LAVEZZO, Rosilene. Determinantes do atraso escolar dos estudantes do Ensino Fundamental I e II. Uma análise estatísticae econométrica para o Brasil. 120f. Dissertação Programa de Pós-Graduação Interdisciplinar Sociedade e Desenvolvimento. Universidade Estadual do Paraná (UNESPAR). Campus de Campo Mourão. Campo Mourão, 2019.',
-        icon: 'success',
-        button: 'Ciente'
-    });
-});
